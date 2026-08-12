d7edf16a9328d39309d94ac7a7297b2a24547ed7
tyagiakshara016-glitch <tyagiakshara016@gmail.com>
2026-08-12 21:56:20 +0530

    ✅ MUL123 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-12

---
+++ codechef/medium/MUL123/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T --> 0){
+		    int N = sc.nextInt();
+		    
+		}
+
+	}
+}
