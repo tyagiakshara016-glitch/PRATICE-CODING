a8148c6d5c878ae61cdd683364c95f6b36dfc40f
tyagiakshara016-glitch <tyagiakshara016@gmail.com>
2026-08-12 21:53:53 +0530

    ✅ BUSROW | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/BUSROW/solution.java
+++ codechef/medium/BUSROW/solution.java
@@ -7,6 +7,20 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T -->0){
+		    int N = sc.nextInt();
+		    int M = sc.nextInt();
+		    int X = sc.nextInt();
+		    
+            int row = (X - 1) / M + 1;
+
+            int fromFront = row;
+            int fromBack = N - row + 1;
+
+            System.out.println(Math.min(fromFront, fromBack));
+		}
 
 	}
 }
