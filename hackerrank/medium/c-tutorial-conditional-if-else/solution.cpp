44d323ebfdf347637d7cc513f3374b5e123b2fc4
tyagiakshara016-glitch <tyagiakshara016@gmail.com>
2026-08-17 12:26:00 +0530

    ✅ Basic Data Types | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-08-17

---
+++ hackerrank/medium/c-tutorial-conditional-if-else/solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    long b;
+    long a;
+    
+    char c; 
+    double d;
+    double e;
+    cin>>b;
+    cin>>a;
+    cin>>c;
+    cin>>d;
+    cin>>e;
+    cout<<b<<endl;
+    cout<<a<<endl;
+    cout<<c<<endl;
+    cout<<fixed<<d<<endl;
+    cout<<fixed<<e<<endl;
+    return 0;
+}
