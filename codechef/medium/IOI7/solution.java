794af4859f85f9ecbf290f15a76bd54fc5c5cc16
tyagiakshara016-glitch <tyagiakshara016@gmail.com>
2026-08-19 21:32:13 +0530

    ✅ IOI7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ codechef/medium/IOI7/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int G = sc.nextInt();
+		if(N>=G){
+		    System.out.print("Yes");
+		}
+		else{
+		    System.out.print("No");
+		}
+
+	}
+}
