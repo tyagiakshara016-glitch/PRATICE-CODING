fd1437df5a0455635b414a01f5b4f122ef370c15
tyagiakshara016-glitch <tyagiakshara016@gmail.com>
2026-09-24 12:14:17 +0530

    ✅ Club Member Record Card | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-24

---
+++ hackerrank/medium/club-member-record-card/solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String name = "Arun";
+        int memberID = 101;
+        String branch = "CSE";
+        System.out.println("Name: "+name);
+        System.out.println("Member ID: "+memberID);
+        System.out.println("Branch: "+branch);
+    }
+}
